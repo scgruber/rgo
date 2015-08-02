4d605ee04230df22fc8745465043bf3f5e122d59
Sam Gruber <grubermensch@gmail.com>
2015-08-01 20:16:43 -0700

    Use GraphModel as the internal store for GobanModel

--- src/jsx/models/goban.jsx
+++ src/jsx/models/goban.jsx
@@ -1,14 +1,35 @@
+var GraphModel = require('./graph.jsx');
+
 var GobanModel = function(size) {
   this.size = size
-  this._board = Array.apply(null, Array(size*size)).map(function() { return 0; });
+  nodes = [].concat.apply([],
+      Array.apply(null, Array(size)).map(function(v, rowIdx) {
+      return Array.apply(null, Array(size)).map(function(v, colIdx) {
+        return { name: toNodeName([colIdx, rowIdx]), value: 0 };
+      });
+    })
+  );
+  edges = [].concat.apply([],
+    nodes.map(function(n) {
+      var coords = fromNodeName(n.name);
+      var colIdx = coords[0], rowIdx = coords[1];
+      var west  = ((colIdx != 0     ) ? toNodeName([colIdx-1, rowIdx  ]) : null);
+      var east  = ((colIdx != size-1) ? toNodeName([colIdx+1, rowIdx  ]) : null);
+      var north = ((rowIdx != 0     ) ? toNodeName([colIdx,   rowIdx-1]) : null);
+      var south = ((rowIdx != size-1) ? toNodeName([colIdx,   rowIdx+1]) : null);
+      var targets = ([west,east,north,south]).filter(function(t) { return t != null; });
+      return targets.map(function(t) { return [n.name, t]});
+    })
+  );
+  this._board = new GraphModel(nodes, edges);
 }
 
-var toLinearIndex = function(coords,size) {
-  return coords[0]*size + coords[1];
+var toNodeName = function(coords) {
+  return coords[0] + "-" + coords[1];
 }
 
-var fromLinearIndex = function(idx, size) {
-  return [idx/size, idx%size];
+var fromNodeName = function(node_name) {
+  return node_name.split("-").map(function(idx) { return parseInt(idx); });
 }
 
 var checkInBounds = function(x, y, size) {
@@ -19,21 +40,21 @@ var checkInBounds = function(x, y, size) {
 
 GobanModel.prototype.clone = function() {
   var clone = new GobanModel(this.size);
-  clone._board = this._board;
+  clone._board = this._board.clone();
   return clone;
 }
 
 GobanModel.prototype.get = function(x,y) {
   checkInBounds(x,y,this.size);
 
-  return this._board[toLinearIndex([x,y], this.size)];
+  return this._board.node(toNodeName([x,y])).value;
 }
 
 GobanModel.prototype.set = function(x,y,value) {
   checkInBounds(x,y,this.size);
 
   var clone = this.clone();
-  clone._board[toLinearIndex([x,y], this.size)] = value;
+  clone._board.updateNode(toNodeName([x,y]), { value: value });
   return clone;
 }
 
